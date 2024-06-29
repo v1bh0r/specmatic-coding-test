bb64ba77d02380314a8ac5121f83b6d7a6b36f4a
Vibhor Mahajan <vmahajan@lendingpoint.com>
2024-06-29 15:45:38 +0530

    A clean working environment

--- src/main/kotlin/com/store/controllers/Products.kt
+++ src/main/kotlin/com/store/controllers/Products.kt
@@ -1,7 +1,21 @@
 package com.store.controllers
 
+import org.openapitools.api.ProductsApi
+import org.openapitools.model.Product
+import org.openapitools.model.ProductDetails
+import org.openapitools.model.ProductId
+import org.openapitools.model.ProductType
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.RestController
 
-
 @RestController
-class Products
+open class Products : ProductsApi {
+    override fun productsGet(type: ProductType?): ResponseEntity<List<Product>> {
+        return ResponseEntity.ok(listOf(Product(1, "XYZ Phone", ProductType.gadget, 2)))
+    }
+
+    override fun productsPost(productDetails: ProductDetails?): ResponseEntity<ProductId> {
+        // Return a 201
+        return ResponseEntity.status(201).body(ProductId(1))
+    }
+}
